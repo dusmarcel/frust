d9887868dd421adc947f00ba1a4eabe0cba3403a
marceldus <marcel@keienb.org>
2024-08-19 22:16:20 +0200

    added major components

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,26 @@ use yew::prelude::*;
 
 #[function_component]
 fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
-    };
-
     html! {
-        <div>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
-        </div>
+        <>
+            <h1>{"Willkommen bei Frust!"}</h1>
+            <OpenFile />
+            <FileView />
+        </>
+    }
+}
+
+#[function_component]
+fn OpenFile() -> Html {
+    html! {
+        <p>{"Open File..."}</p>
+    }
+}
+
+#[function_component]
+fn FileView() -> Html {
+    html! {
+        <p>{"View File..."}</p>
     }
 }
 
