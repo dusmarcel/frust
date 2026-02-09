0b921bd841f4475533b98cef619bfc638433035f
dusmarcel <marcel@keienb.org>
2026-02-09 02:48:01 +0100

    fixed title & icon for desktop app

--- packages/desktop/src/main.rs
+++ packages/desktop/src/main.rs
@@ -1,4 +1,5 @@
 use dioxus::prelude::*;
+use image::ImageReader;
 
 use views::Home;
 
@@ -12,7 +13,13 @@ enum Route {
 }
 
 fn main() {
-    dioxus::launch(App);
+    dioxus::LaunchBuilder::desktop()
+        .with_cfg(
+            dioxus::desktop::Config::new()
+                .with_window(dioxus::desktop::WindowBuilder::new().with_title("Frust"))
+                .with_icon(load_icon()),
+        )
+        .launch(App);
 }
 
 #[component]
@@ -31,4 +38,18 @@ fn DesktopNavbar() -> Element {
     rsx! {
         Outlet::<Route> {}
     }
+}
+
+fn load_icon() -> dioxus::desktop::tao::window::Icon {
+    const ICON_BYTES: &[u8] = include_bytes!("../../web/assets/favicon.ico");
+
+    let decoder = ImageReader::new(std::io::Cursor::new(ICON_BYTES))
+        .with_guessed_format()
+        .expect("failed to detect favicon format");
+    let image = decoder.decode().expect("failed to decode favicon");
+    let rgba = image.into_rgba8();
+    let (width, height) = rgba.dimensions();
+
+    dioxus::desktop::tao::window::Icon::from_rgba(rgba.into_raw(), width, height)
+        .expect("failed to build desktop icon")
 }
